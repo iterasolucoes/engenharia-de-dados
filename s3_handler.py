d37314d15a13f103a38c4d2a9db3be47d4b550f2
Dhiogo Corrêa <dhiogo.sa@usp.br>
2019-09-23 20:45:43 -0300

    Update s3_handler.py

--- s3_handler.py
+++ s3_handler.py
@@ -2,6 +2,12 @@ import boto3
 import json
 
 
+## Link de login do console: https://981601120657.signin.aws.amazon.com/console
+## Usuario: EngDados
+## Senha: engdados@2019
+
+## https://s3.console.aws.amazon.com/s3/buckets/minicurso-engenharia-de-dados/?region=us-east-1&tab=overview
+
 client = boto3.client(
     's3',
     aws_access_key_id='AKIA6JC74OGIXONPB5UP',
